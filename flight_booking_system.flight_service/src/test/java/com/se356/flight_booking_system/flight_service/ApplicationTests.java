56b5b95828962cde6557c6b01ab639c3f94def19
chabuuuu <haphuthinh332004@gmail.com>
2025-05-06 13:32:04 +0700

    build(source): init

---
+++ flight_booking_system.flight_service/src/test/java/com/se356/flight_booking_system/flight_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se356.flight_booking_system.flight_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
